dc2ba21780aabd685f2928e98a29a8d5ca16a088
Adam Saegebarth <adams@synapse.com>
2012-07-09 16:52:14 -0700

    remove extra cert line
    Signed-off-by: Adam Saegebarth <adams@synapse.com>

--- manifests/config/apache2.pp
+++ manifests/config/apache2.pp
@@ -16,9 +16,12 @@ define ssl::config::apache2 (
   }
 
   if ! $link_to {
-    ssl::key[$tls_key]                      ~> Service['apache2']
-    ssl::cert[$name]                        ~> Service['apache2']
-    if $tls_ca    { ssl::cert[$tls_ca]      ~> Service['apache2'] }
-    if $tls_chain { ssl::chain[$tls_chain]  ~> Service['apache2'] }
+    ssl::key[$tls_key] ~> Service['apache2']
+    if $tls_chain { 
+      ssl::cert[$tls_ca] ~> Service['apache2'] 
+    }
+    if $tls_chain { 
+      ssl::chain[$tls_chain]  ~> Service['apache2'] 
+    }
   }
 }
