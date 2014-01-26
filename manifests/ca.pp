d1804b15d67d2057bcf5f06bf9c53ce8d2a97ed0
kingcody <me@codymize.com>
2014-01-25 21:40:38 -0500

    using prefix in 'puppet_managed' folder

--- manifests/ca.pp
+++ manifests/ca.pp
@@ -7,7 +7,14 @@ define ssl::ca (
     local_cert_install_dir => $local_cert_install_dir,
   }
 
-  file { "${ssl::params::ssl_local_certs}/${local_cert_install_dir}/${name}.crt" :
+  if $local_cert_install_dir == 'puppet_managed' {
+    $prefix = "ca_"
+  }
+  else {
+    $prefix = ""
+  }
+
+  file { "${ssl::params::ssl_local_certs}/${local_cert_install_dir}/${prefix}${name}.crt" :
     ensure  => file,
     mode    => '0444',
     group   => 'ssl-cert',
